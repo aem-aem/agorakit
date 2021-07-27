a691971b209966119dff1f5d42b91aa7f093dca2
Cloud User <centos@vps820140.ovh.net>
2021-07-27 14:57:09 +0200

    correct receurrent event creation in db_maintenance

--- agorakit/db_maintenance.sh
+++ agorakit/db_maintenance.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 minute=$(date '+%M')
+minute=${minute#0}
 hour=$(date '+%k')
+hour=${hour#0}
 day=$(date '+%w')
 dayOfMonth=$(date '+%d')
 dayOfMonthMod2=$(($dayOfMonth % 2))
@@ -14,8 +16,12 @@ if [[ (( $hour -eq 0 || $hour -eq 6 || $hour -eq 12 || $hour -eq 18 )) && $minut
 	mysql $MYSQL_OPTS -e "update membership set notification_interval = 1440; update groups set group_type=2; update membership set notification_interval=-1 where user_id<3;"
 fi
 
-mysql $MYSQL_OPTS -e "insert into actions select uniq+number, a.created_at, a.updated_at,deleted_at, a.group_id, a.user_id, a.name, a.body, a.start + interval 4*number week, a.stop + interval 4*number week, a.location, a.latitude, a.longitude from (select substring(regexp_replace(md5(concat(group_id, user_id, name)), '[a-z]', ''),1,6) as uniq, min(id) as id from actions where name like '\%[MENSUEL]' group by 1) seed, actions as a, range_6 where seed.id=a.id;"
+for freq in "HEBDO" "MENSUEL"; do
+	mysql $MYSQL_OPTS -e "delete a from actions a join (select group_id, name, user_id, id from actions where name like '%["$freq"]' and body like '[COPY]%') seed on a.id=seed.id and a.group_id=seed.group_id and a.name=seed.name and a.user_id=seed.user_id;"
 
-mysql $MYSQL_OPTS -e "insert into actions select uniq+number, a.created_at, a.updated_at,deleted_at, a.group_id, a.user_id, a.name, a.body, a.start + interval number week, a.stop + interval number week, a.location, a.latitude, a.longitude from (select substring(regexp_replace(md5(concat(group_id, user_id, name)), '[a-z]', ''),1,6) as uniq, min(id) as id from actions where name like '\%[HEBDO]' group by 1) seed, actions as a, range_26 where seed.id=a.id;"
+	mysql $MYSQL_OPTS -e "insert into actions (created_at, updated_at, deleted_at, group_id, user_id, name, body, start, stop, location, latitude, longitude) select a.created_at, a.updated_at, a.deleted_at, a.group_id, a.user_id, a.name, concat('[COPY]<br><br>', a.body), a.start + interval number week, a.stop + interval number week, a.location, a.latitude, a.longitude from (select group_id, name, user_id, min(id) as id from actions where name like '%["$freq"]' and body not like '[COPY]%' group by 1) seed, actions as a, weeks where seed.id=a.id and weeks.freq='"$freq"';"
+done
+
+#mysql $MYSQL_OPTS -e "insert into actions select uniq+number, a.created_at, a.updated_at,deleted_at, a.group_id, a.user_id, a.name, a.body, a.start + interval number week, a.stop + interval number week, a.location, a.latitude, a.longitude from (select substring(regexp_replace(md5(concat(group_id, user_id, name)), '[a-z]', ''),1,6) as uniq, min(id) as id from actions where name like '%[HEBDO]' group by 1) seed, actions as a, range_26 where seed.id=a.id;"
 
 mysql $MYSQL_OPTS --skip-column-names -B -e "select distinct settings from groups;" | python3 /reset_permissions.py | mysql $MYSQL_OPTS
