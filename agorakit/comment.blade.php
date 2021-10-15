9003e3ece39d70f4a0aab78db97452e276989408
Cloud User <centos@vps820140.ovh.net>
2021-10-15 11:10:54 +0200

    modify comment view so group admin can delete a comment

---
+++ agorakit/comment.blade.php
@@ -0,0 +1,66 @@
+<a name="comment_{{$comment->id}}"></a>
+
+<div class="mb-6 pb-6 comment">
+
+    <div class="flex">
+
+        @include('users.avatar', ['user' => $comment->user])
+
+        <div class="w-100 flex-grow ml-4 @if ($comment->isRead) overflow-hidden text-gray-700 h-12 read @endif">
+            {{-- <--- TODO here me manage collapsed comments --}}
+            <div>
+                {!! filter($comment->body) !!}
+            </div>
+
+
+
+            <div class="text-xs text-gray-600">
+
+                <a up-follow href="{{ route('users.show', [$comment->user]) }}">{{$comment->user->name}}</a>
+
+                {{ dateForHumans($comment->created_at) }}
+            </div>
+
+            <div>
+                <x-reactions :model="$comment" />
+            </div>
+        </div>
+
+
+
+        @can('delete', $comment)
+        <div class="dropdown">
+            <a class="rounded-full hover:bg-gray-400 w-10 h-10 flex items-center justify-center type=" button"
+                id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-ellipsis-h"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+
+                @can('update', $comment)
+                <a class="dropdown-item"
+                    href="{{ action('CommentController@edit', [$comment->discussion->group, $comment->discussion, $comment]) }}"><i
+                        class="fa fa-pencil"></i>
+                    {{trans('messages.edit')}}
+                </a>
+                @endcan
+
+                @can('delete', $comment)
+                <a class="dropdown-item" up-modal=".dialog"
+                    href="{{ action('CommentController@destroyConfirm', [$comment->discussion->group, $comment->discussion, $comment]) }}"><i
+                        class="fa fa-trash"></i>
+                    {{trans('messages.delete')}}
+                </a>
+                @endcan
+                <a class="dropdown-item"
+                    href="{{action('CommentController@history', [$comment->discussion->group, $comment->discussion, $comment])}}"><i
+                        class="fa fa-history"></i> {{trans('messages.show_history')}}</a>
+            </div>
+        </div>
+        @endcan
+
+    </div>
+
+
+
+
+</div>
