50d1069cede0bdc2cd5ed1b1799e9b40d93991b6
Cloud User <centos@vps820140.ovh.net>
2021-12-06 19:40:49 +0100

    add mega backup

--- agorakit/backup.sh
+++ agorakit/backup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mega-put /opt/agorakit/storage/app /opt/agorakit/storage/logs /opt/agorakit/storage/dump.sql agora.coook.co.uk/
 aws s3 sync /opt/agorakit/storage/app s3://$S3_BACKUP_BUCKET_NAME/app
 aws s3 sync /opt/agorakit/storage/logs s3://$S3_BACKUP_BUCKET_NAME/logs
 aws s3 cp /opt/agorakit/storage/dump.sql s3://$S3_BACKUP_BUCKET_NAME/
