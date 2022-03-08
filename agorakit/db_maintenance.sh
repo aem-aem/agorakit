8516c2a9b7e294aabffc212dee15d8057012087f
Cloud User <centos@vps820140.ovh.net>
2022-03-08 10:30:44 +0100

    correct reset_permission.py script to handle custom tab

--- agorakit/db_maintenance.sh
+++ agorakit/db_maintenance.sh
@@ -6,7 +6,7 @@ hour=$(date '+%k')
 hour=${hour#0}
 day=$(date '+%w')
 dayOfMonth=$(date '+%d')
-dayOfMonthMod2=$(($dayOfMonth % 2))
+dayOfMonthMod2=$((${dayOfMonth#0} % 2))
 
 MYSQL_OPTS="-u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE" 
 
@@ -17,7 +17,7 @@ if [[ (( $hour -eq 0 || $hour -eq 6 || $hour -eq 12 || $hour -eq 18 )) && $minut
 fi
 
 for freq in "HEBDO" "MENSUEL"; do
-	mysql $MYSQL_OPTS -e "insert into actions (created_at, updated_at, deleted_at, group_id, user_id, name, body, start, stop, location, latitude, longitude) select a.created_at, a.updated_at, a.deleted_at, a.group_id, a.user_id, a.name, concat('[COPY]<br><br>', a.body), a.start + interval number week, a.stop + interval number week, a.location, a.latitude, a.longitude from (select group_id, name, user_id, min(id) as id from actions where name like '%["$freq"]' and body not like '[COPY]%' group by 1) seed, actions as a, weeks where seed.created_at + interval 5 minute >= now() and seed.id=a.id and weeks.freq='"$freq"';"
+	mysql $MYSQL_OPTS -e "insert into actions (created_at, updated_at, deleted_at, group_id, user_id, name, body, start, stop, location, latitude, longitude) select a.created_at, a.updated_at, a.deleted_at, a.group_id, a.user_id, a.name, concat('[COPY]<br><br>', a.body), a.start + interval number week, a.stop + interval number week, a.location, a.latitude, a.longitude from (select created_at, group_id, name, user_id, min(id) as id from actions where name like '%["$freq"]' and body not like '[COPY]%' group by 1) seed, actions as a, weeks where seed.created_at + interval 5 minute >= now() and seed.id=a.id and weeks.freq='"$freq"';"
 done
 
 mysql $MYSQL_OPTS --skip-column-names -B -e "select distinct settings from groups;" | python3 /reset_permissions.py | mysql $MYSQL_OPTS
--- agorakit/reset_permissions.py
+++ agorakit/reset_permissions.py
@@ -2,11 +2,11 @@ import fileinput
 import json
 
 for line in fileinput.input():
-    line = line.strip()
+    line = line.strip().replace('\\\\\"', '\\\\\\\"')
     _dict = json.loads(line)
     _dict['permissions'] = {'member': ['create-discussion', 'create-file', 'create-action']}
     _dict['custom_permissions'] = True
-    new_line = json.dumps(_dict)
+    new_line = json.dumps(_dict).replace('\\\\\\\"', '\\\\\"')
     if line != new_line:
         print(f"update groups set settings='{new_line}' where settings='{line.strip()}';")
 
