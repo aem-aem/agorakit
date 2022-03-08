3363866f68cb1f1cfa53ef0400990591a7937633
Cloud User <centos@vps820140.ovh.net>
2022-03-08 09:34:31 +0100

    do not duplicate events unless created less than 5 minutes ago + chown log directory

--- agorakit/db_maintenance.sh
+++ agorakit/db_maintenance.sh
@@ -17,11 +17,7 @@ if [[ (( $hour -eq 0 || $hour -eq 6 || $hour -eq 12 || $hour -eq 18 )) && $minut
 fi
 
 for freq in "HEBDO" "MENSUEL"; do
-	mysql $MYSQL_OPTS -e "delete a from actions a join (select group_id, name, user_id, id from actions where name like '%["$freq"]' and body like '[COPY]%') seed on a.id=seed.id and a.group_id=seed.group_id and a.name=seed.name and a.user_id=seed.user_id;"
-
-	mysql $MYSQL_OPTS -e "insert into actions (created_at, updated_at, deleted_at, group_id, user_id, name, body, start, stop, location, latitude, longitude) select a.created_at, a.updated_at, a.deleted_at, a.group_id, a.user_id, a.name, concat('[COPY]<br><br>', a.body), a.start + interval number week, a.stop + interval number week, a.location, a.latitude, a.longitude from (select group_id, name, user_id, min(id) as id from actions where name like '%["$freq"]' and body not like '[COPY]%' group by 1) seed, actions as a, weeks where seed.id=a.id and weeks.freq='"$freq"';"
+	mysql $MYSQL_OPTS -e "insert into actions (created_at, updated_at, deleted_at, group_id, user_id, name, body, start, stop, location, latitude, longitude) select a.created_at, a.updated_at, a.deleted_at, a.group_id, a.user_id, a.name, concat('[COPY]<br><br>', a.body), a.start + interval number week, a.stop + interval number week, a.location, a.latitude, a.longitude from (select group_id, name, user_id, min(id) as id from actions where name like '%["$freq"]' and body not like '[COPY]%' group by 1) seed, actions as a, weeks where seed.created_at + interval 5 minute >= now() and seed.id=a.id and weeks.freq='"$freq"';"
 done
-
-#mysql $MYSQL_OPTS -e "insert into actions select uniq+number, a.created_at, a.updated_at,deleted_at, a.group_id, a.user_id, a.name, a.body, a.start + interval number week, a.stop + interval number week, a.location, a.latitude, a.longitude from (select substring(regexp_replace(md5(concat(group_id, user_id, name)), '[a-z]', ''),1,6) as uniq, min(id) as id from actions where name like '%[HEBDO]' group by 1) seed, actions as a, range_26 where seed.id=a.id;"
 
 mysql $MYSQL_OPTS --skip-column-names -B -e "select distinct settings from groups;" | python3 /reset_permissions.py | mysql $MYSQL_OPTS
